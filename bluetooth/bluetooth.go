4230bdb3031bc84124ae304e4ffcbb3b568e0018
Quinn Casey <qcasey1@gmail.com>
2019-03-28 10:25:48 -0700

    Filling out all BT dbus functionality

--- bluetooth/bluetooth.go
+++ bluetooth/bluetooth.go
@@ -2,20 +2,16 @@ package bluetooth
 
 import (
 	"encoding/json"
-	"errors"
-	"flag"
+	"log"
 	"net/http"
+	"os/exec"
 	"strings"
-
-	"github.com/godbus/dbus"
 )
 
 var btAddress string
 
-func init() {
-	flag.StringVar(&btAddress, "bt-device", "", "Bluetooth Media device to accept as default")
-	flag.Parse()
-
+// SetAddress makes address given in args available to all dbus functions
+func SetAddress(address string) {
 	// Format address for dbus
 	if btAddress != "" {
 		btAddress = strings.Replace(btAddress, ":", "_", -1)
@@ -24,66 +20,86 @@ func init() {
 	}
 }
 
-// fetches a dbus object
-func _getObject() (dbus.BusObject, error) {
-	if btAddress != "" {
-		conn, err := dbus.SessionBus()
-
-		if err != nil {
-			return nil, err
-		}
-
-		obj := conn.Object("org.bluez", dbus.ObjectPath("/org/bluez/hci0/dev_4C_32_75_AD_98_24/player0"))
-		return obj, nil
-	}
-
-	return nil, errors.New("Invalid BT Address: " + btAddress)
-}
-
 // Connect new bluetooth device
 func Connect(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode("test")
+	out, err := exec.Command("dbus-send", "--system", "--print-reply", "--type=method_call", "--dest=org.bluez", "/org/bluez/hci0/dev_"+btAddress+"/player0", "org.bluez.Device1.Connect").Output()
+
+	if err != nil {
+		log.Println(err)
+		json.NewEncoder(w).Encode(err)
+	} else {
+		json.NewEncoder(w).Encode(out)
+	}
 }
 
 // GetDeviceInfo attempts to get metadata about connected device
 func GetDeviceInfo(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode("test")
+	out, err := exec.Command("dbus-send", "--system", "--print-reply", "--type=method_call", "--dest=org.bluez", "/org/bluez/hci0/dev_"+btAddress+"/player0", "org.freedesktop.DBus.Properties.Get", "string:org.bluez.MediaPlayer1", "string:Status").Output()
+
+	if err != nil {
+		log.Println(err)
+		json.NewEncoder(w).Encode(err)
+	} else {
+		json.NewEncoder(w).Encode(out)
+	}
 }
 
 // GetMediaInfo attempts to get metadata about current track
 func GetMediaInfo(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode("test")
+	out, err := exec.Command("dbus-send", "--system", "--print-reply", "--type=method_call", "--dest=org.bluez", "/org/bluez/hci0/dev_"+btAddress+"/player0", "org.freedesktop.DBus.Properties.Get", "string:org.bluez.MediaPlayer1", "string:Track").Output()
+
+	if err != nil {
+		log.Println(err)
+		json.NewEncoder(w).Encode(err)
+	} else {
+		json.NewEncoder(w).Encode(out)
+	}
 }
 
 // Prev skips to previous track
 func Prev(w http.ResponseWriter, r *http.Request) {
-	obj, err := _getObject()
+	out, err := exec.Command("dbus-send", "--system", "--print-reply", "--type=method_call", "--dest=org.bluez", "/org/bluez/hci0/dev_"+btAddress+"/player0", "org.bluez.MediaPlayer1.Previous").Output()
 
 	if err != nil {
+		log.Println(err)
 		json.NewEncoder(w).Encode(err)
 	} else {
-		var s string
-		err = obj.Call("org.bluez.MediaPlayer1.Previous", 0).Store(&s)
-
-		if err != nil {
-			json.NewEncoder(w).Encode(err)
-		} else {
-			json.NewEncoder(w).Encode(s)
-		}
+		json.NewEncoder(w).Encode(out)
 	}
 }
 
 // Next skips to next track
 func Next(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode("test")
+	out, err := exec.Command("dbus-send", "--system", "--print-reply", "--type=method_call", "--dest=org.bluez", "/org/bluez/hci0/dev_"+btAddress+"/player0", "org.bluez.MediaPlayer1.Next").Output()
+
+	if err != nil {
+		log.Println(err)
+		json.NewEncoder(w).Encode(err)
+	} else {
+		json.NewEncoder(w).Encode(out)
+	}
 }
 
 // Play attempts to play bluetooth media
 func Play(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode("test")
+	out, err := exec.Command("dbus-send", "--system", "--print-reply", "--type=method_call", "--dest=org.bluez", "/org/bluez/hci0/dev_"+btAddress+"/player0", "org.bluez.MediaPlayer1.Play").Output()
+
+	if err != nil {
+		log.Println(err)
+		json.NewEncoder(w).Encode(err)
+	} else {
+		json.NewEncoder(w).Encode(out)
+	}
 }
 
 // Pause attempts to pause bluetooth media
 func Pause(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode("test")
+	out, err := exec.Command("dbus-send", "--system", "--print-reply", "--type=method_call", "--dest=org.bluez", "/org/bluez/hci0/dev_"+btAddress+"/player0", "org.bluez.MediaPlayer1.Pause").Output()
+
+	if err != nil {
+		log.Println(err)
+		json.NewEncoder(w).Encode(err)
+	} else {
+		json.NewEncoder(w).Encode(out)
+	}
 }
