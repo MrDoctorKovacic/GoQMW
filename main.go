5d768a4cd8d615bd8ac761f4e24c6c41644bb566
Quinn Casey <qcasey1@gmail.com>
2019-03-28 23:08:35 -0700

    More routes

--- main.go
+++ main.go
@@ -7,8 +7,10 @@ import (
 	"log"
 	"net/http"
 
-	b "github.com/MrDoctorKovacic/GoQMW/bluetooth"
-	s "github.com/MrDoctorKovacic/GoQMW/sessions"
+	"github.com/MrDoctorKovacic/GoQMW/external/bluetooth"
+	"github.com/MrDoctorKovacic/GoQMW/external/ping"
+	"github.com/MrDoctorKovacic/GoQMW/external/pybus"
+	"github.com/MrDoctorKovacic/GoQMW/sessions"
 	"github.com/gorilla/mux"
 )
 
@@ -17,15 +19,17 @@ func main() {
 
 	// Start with program arguments
 	var (
-		sqlDatabase string
-		sqlUser     string
-		sqlPassword string
-		btAddress   string
+		sqlDatabase       string
+		sqlUser           string
+		sqlPassword       string
+		btAddress         string
+		remotePingAddress string
 	)
 	flag.StringVar(&sqlDatabase, "database", "", "SQL Database on localhost to log with")
 	flag.StringVar(&sqlUser, "user", "", "SQL Username")
 	flag.StringVar(&sqlPassword, "password", "", "SQL Password")
 	flag.StringVar(&btAddress, "bt-device", "", "Bluetooth Media device to connect and use as default")
+	flag.StringVar(&remotePingAddress, "ping-host", "", "Remote address to fwd pings to")
 	flag.Parse()
 
 	if sqlDatabase != "" && sqlUser != "" && sqlPassword != "" {
@@ -47,35 +51,59 @@ func main() {
 		log.Println("Successfully connected to " + sqlDatabase)
 
 		if sqlEnabled {
-			s.SQLConnect(DB)
+			// Pass DB pool to imports
+			sessions.SQLConnect(DB)
+
+			if remotePingAddress != "" {
+				ping.Setup(DB, remotePingAddress)
+			}
 		}
 	} else {
 		log.Println("Not logging to MySQL.")
 	}
 
 	// Pass argument to its rightful owner
-	b.SetAddress(btAddress)
+	bluetooth.SetAddress(btAddress)
 
 	// Init router
 	router := mux.NewRouter()
 
+	//
+	// Ping routes
+	//
+	router.HandleFunc("/ping/{device}", ping.Ping).Methods("POST")
+
+	//
 	// Session routes
-	router.HandleFunc("/session", s.GetSession).Methods("GET")
-	router.HandleFunc("/session/{Name}", s.GetSessionValue).Methods("GET")
-	router.HandleFunc("/session/{Name}", s.UpdateSessionValue).Methods("POST")
+	//
+	router.HandleFunc("/session", sessions.GetSession).Methods("GET")
+	router.HandleFunc("/session/{name}", sessions.GetSessionValue).Methods("GET")
+	router.HandleFunc("/session/{name}", sessions.SetSessionValue).Methods("POST")
 
-	// PyBus Route
-	router.HandleFunc("/pybus/{command}", s.PyBus).Methods("GET")
+	//
+	// PyBus Routes
+	//
+	router.HandleFunc("/pybus/{command}", pybus.StartPyBusRoutine).Methods("GET")
+	router.HandleFunc("/pybus/restart", pybus.RestartService).Methods("GET")
 
+	//
+	// ALPR Routes
+	//
+	router.HandleFunc("/alpr/{plate}", sessions.LogALPR).Methods("GET")
+	router.HandleFunc("/alpr/restart", sessions.RestartALPR).Methods("GET")
+
+	//
 	// Bluetooth routes
-	router.HandleFunc("/bluetooth", b.GetDeviceInfo).Methods("GET")
-	router.HandleFunc("/bluetooth/getDeviceInfo", b.GetDeviceInfo).Methods("GET")
-	router.HandleFunc("/bluetooth/getMediaInfo", b.GetMediaInfo).Methods("GET")
-	router.HandleFunc("/bluetooth/connect", b.Connect).Methods("GET")
-	router.HandleFunc("/bluetooth/prev", b.Prev).Methods("GET")
-	router.HandleFunc("/bluetooth/next", b.Next).Methods("GET")
-	router.HandleFunc("/bluetooth/pause", b.Pause).Methods("GET")
-	router.HandleFunc("/bluetooth/play", b.Play).Methods("GET")
+	//
+	router.HandleFunc("/bluetooth", bluetooth.GetDeviceInfo).Methods("GET")
+	router.HandleFunc("/bluetooth/getDeviceInfo", bluetooth.GetDeviceInfo).Methods("GET")
+	router.HandleFunc("/bluetooth/getMediaInfo", bluetooth.GetMediaInfo).Methods("GET")
+	router.HandleFunc("/bluetooth/connect", bluetooth.Connect).Methods("GET")
+	router.HandleFunc("/bluetooth/prev", bluetooth.Prev).Methods("GET")
+	router.HandleFunc("/bluetooth/next", bluetooth.Next).Methods("GET")
+	router.HandleFunc("/bluetooth/pause", bluetooth.Pause).Methods("GET")
+	router.HandleFunc("/bluetooth/play", bluetooth.Play).Methods("GET")
+	router.HandleFunc("/bluetooth/restart", bluetooth.RestartService).Methods("GET")
 
 	log.Fatal(http.ListenAndServe(":5353", router))
 }
