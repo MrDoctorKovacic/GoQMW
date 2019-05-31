824c8ccf12f4624d974ddfc74ee8f67561e43089
vugzen-rikxyR-8fabqo <qcasey1@gmail.com>
2019-05-30 23:01:28 -0700

    removing shell

--- external/bluetooth/bluetooth.go
+++ external/bluetooth/bluetooth.go
@@ -118,10 +118,10 @@ func SetAddress(address string) {
 func SendDBusCommand(args []string, hideOutput bool) (string, bool) {
 	if btAddress != "" {
 		// Fill in the meta nonsense
-		newargs := append([]string{"-c", "dbus-send", "--system", "--print-reply", "--type=method_call", "--dest=org.bluez"}, args...)
+		args = append([]string{"--system", "--print-reply", "--type=method_call", "--dest=org.bluez"}, args...)
 		var stderr bytes.Buffer
 		var out bytes.Buffer
-		cmd := exec.Command("/bin/sh", newargs...)
+		cmd := exec.Command("dbus-send", args...)
 		cmd.Stdout = &out
 		cmd.Stderr = &stderr
 		err := cmd.Run()
