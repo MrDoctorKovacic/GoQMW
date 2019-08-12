1f0ebe1ecc9fa466b9ee382b21343f559d3d0eba
Quinn Casey <qcasey1@gmail.com>
2019-08-12 15:26:09 -0700

    Moving GPS out of sessions

---
+++ gps.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/MrDoctorKovacic/MDroid-Core/status"
+)
+
+// GPSData holds various data points we expect to receive
+type GPSData struct {
+	Latitude  string   `json:"latitude,omitempty"`
+	Longitude string   `json:"longitude,omitempty"`
+	Time      string   `json:"time,omitempty"` // This will help measure latency :)
+	Altitude  *float32 `json:"altitude,omitempty"`
+	EPV       *float32 `json:"epv,omitempty"`
+	EPT       *float32 `json:"ept,omitempty"`
+	Speed     *float32 `json:"speed,omitempty"`
+	Climb     *float32 `json:"climb,omitempty"`
+}
+
+// GPS is the last posted GPS fix
+var GPS GPSData
+
+//
+// GPS Functions
+//
+
+// GetGPSValue returns the latest GPS fix
+func GetGPSValue(w http.ResponseWriter, r *http.Request) {
+	// Log if requested
+	if VerboseOutput {
+		SessionStatus.Log(status.OK(), "Responding to GET request for all GPS values")
+	}
+	json.NewEncoder(w).Encode(GPS)
+}
+
+// SetGPSValue posts a new GPS fix
+func SetGPSValue(w http.ResponseWriter, r *http.Request) {
+	var newdata GPSData
+	_ = json.NewDecoder(r.Body).Decode(&newdata)
+
+	// Log if requested
+	if VerboseOutput {
+		SessionStatus.Log(status.OK(), "Responding to POST request for gps values")
+	}
+
+	// Prepare new value
+	var postingString strings.Builder
+
+	// Update value for global session if the data is newer (not nil)
+	// Can't find a better way to go about this
+	if newdata.Latitude != "" {
+		GPS.Latitude = newdata.Latitude
+		postingString.WriteString(fmt.Sprintf("latitude=\"%s\",", newdata.Latitude))
+	}
+	if newdata.Longitude != "" {
+		GPS.Longitude = newdata.Longitude
+		postingString.WriteString(fmt.Sprintf("longitude=\"%s\",", newdata.Longitude))
+	}
+	if newdata.Altitude != nil {
+		GPS.Altitude = newdata.Altitude
+		log.Println(fmt.Sprintf("%f", *newdata.Altitude))
+		postingString.WriteString(fmt.Sprintf("altitude=%f,", *newdata.Altitude))
+	}
+	if newdata.Speed != nil {
+		GPS.Speed = newdata.Speed
+		postingString.WriteString(fmt.Sprintf("speed=%f,", *newdata.Speed))
+	}
+	if newdata.Climb != nil {
+		GPS.Climb = newdata.Climb
+		postingString.WriteString(fmt.Sprintf("climb=%f,", *newdata.Climb))
+	}
+	if newdata.Time != "" {
+		GPS.Time = newdata.Time
+	}
+	if newdata.EPV != nil {
+		GPS.EPV = newdata.EPV
+		postingString.WriteString(fmt.Sprintf("EPV=%f,", *newdata.EPV))
+	}
+	if newdata.EPT != nil {
+		GPS.EPT = newdata.EPT
+		postingString.WriteString(fmt.Sprintf("EPT=%f,", *newdata.EPT))
+	}
+
+	// Insert into database
+	if Config.DatabaseEnabled {
+		err := DB.Write(fmt.Sprintf("gps %s", strings.TrimSuffix(postingString.String(), ",")))
+
+		if err != nil {
+			SessionStatus.Log(status.Error(), "Error writing string "+postingString.String()+" to influx DB: "+err.Error())
+		} else {
+			SessionStatus.Log(status.OK(), "Logged "+postingString.String()+" to database")
+		}
+	}
+
+	json.NewEncoder(w).Encode("OK")
+}
--- sessions.go
+++ sessions.go
@@ -30,24 +30,9 @@ type SessionData struct {
 	LastUpdate string `json:"lastUpdate,omitempty"`
 }
 
-// GPSData holds various data points we expect to receive
-type GPSData struct {
-	Latitude  string   `json:"latitude,omitempty"`
-	Longitude string   `json:"longitude,omitempty"`
-	Time      string   `json:"time,omitempty"` // This will help measure latency :)
-	Altitude  *float32 `json:"altitude,omitempty"`
-	EPV       *float32 `json:"epv,omitempty"`
-	EPT       *float32 `json:"ept,omitempty"`
-	Speed     *float32 `json:"speed,omitempty"`
-	Climb     *float32 `json:"climb,omitempty"`
-}
-
 // Session is the global session accessed by incoming requests
 var Session map[string]SessionData
 var sessionLock sync.Mutex
-
-// GPS is the last posted GPS fix
-var GPS GPSData
 
 // Session WebSocket upgrader
 var upgrader = websocket.Upgrader{} // use default options
@@ -259,79 +244,4 @@ func SetSessionRawValue(name string, value string) {
 	var newdata SessionData
 	newdata.Value = value
 	SetSessionValue(name, newdata, true)
-}
-
-//
-// GPS Functions
-//
-
-// GetGPSValue returns the latest GPS fix
-func GetGPSValue(w http.ResponseWriter, r *http.Request) {
-	// Log if requested
-	if VerboseOutput {
-		SessionStatus.Log(status.OK(), "Responding to GET request for all GPS values")
-	}
-	json.NewEncoder(w).Encode(GPS)
-}
-
-// SetGPSValue posts a new GPS fix
-func SetGPSValue(w http.ResponseWriter, r *http.Request) {
-	var newdata GPSData
-	_ = json.NewDecoder(r.Body).Decode(&newdata)
-
-	// Log if requested
-	if VerboseOutput {
-		SessionStatus.Log(status.OK(), "Responding to POST request for gps values")
-	}
-
-	// Prepare new value
-	var postingString strings.Builder
-
-	// Update value for global session if the data is newer (not nil)
-	// Can't find a better way to go about this
-	if newdata.Latitude != "" {
-		GPS.Latitude = newdata.Latitude
-		postingString.WriteString(fmt.Sprintf("latitude=\"%s\",", newdata.Latitude))
-	}
-	if newdata.Longitude != "" {
-		GPS.Longitude = newdata.Longitude
-		postingString.WriteString(fmt.Sprintf("longitude=\"%s\",", newdata.Longitude))
-	}
-	if newdata.Altitude != nil {
-		GPS.Altitude = newdata.Altitude
-		log.Println(fmt.Sprintf("%f", *newdata.Altitude))
-		postingString.WriteString(fmt.Sprintf("altitude=%f,", *newdata.Altitude))
-	}
-	if newdata.Speed != nil {
-		GPS.Speed = newdata.Speed
-		postingString.WriteString(fmt.Sprintf("speed=%f,", *newdata.Speed))
-	}
-	if newdata.Climb != nil {
-		GPS.Climb = newdata.Climb
-		postingString.WriteString(fmt.Sprintf("climb=%f,", *newdata.Climb))
-	}
-	if newdata.Time != "" {
-		GPS.Time = newdata.Time
-	}
-	if newdata.EPV != nil {
-		GPS.EPV = newdata.EPV
-		postingString.WriteString(fmt.Sprintf("EPV=%f,", *newdata.EPV))
-	}
-	if newdata.EPT != nil {
-		GPS.EPT = newdata.EPT
-		postingString.WriteString(fmt.Sprintf("EPT=%f,", *newdata.EPT))
-	}
-
-	// Insert into database
-	if Config.DatabaseEnabled {
-		err := DB.Write(fmt.Sprintf("gps %s", strings.TrimSuffix(postingString.String(), ",")))
-
-		if err != nil {
-			SessionStatus.Log(status.Error(), "Error writing string "+postingString.String()+" to influx DB: "+err.Error())
-		} else {
-			SessionStatus.Log(status.OK(), "Logged "+postingString.String()+" to database")
-		}
-	}
-
-	json.NewEncoder(w).Encode("OK")
 }
