a9dac1bf7af18b1d17881e41de6c5076de00fb53
Quinn Casey <qcasey1@gmail.com>
2019-08-12 15:24:31 -0700

    Main common utils

--- utils/utils.go
+++ utils/utils.go
@@ -1,8 +1,52 @@
 package utils
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+	"unicode"
+)
 
 // FormatName returns string in upper case with underscores replacing spaces
-func FormatName(text string) string {
-	return strings.ToUpper(strings.Replace(text, " ", "_", -1))
+func FormatName(name string) string {
+	return strings.TrimSpace(strings.ToUpper(strings.Replace(name, " ", "_", -1)))
+}
+
+// IsValidName verifies the name is alphanumeric
+func IsValidName(name string) bool {
+	for _, r := range name {
+		if !unicode.IsLetter(r) && !unicode.IsNumber(r) {
+			return false
+		}
+	}
+	return true
+}
+
+// IsPositiveRequest helps translate UP or LOCK into true or false
+func IsPositiveRequest(request string) (bool, error) {
+	switch request {
+	case "ON":
+		fallthrough
+	case "UP":
+		fallthrough
+	case "LOCK":
+		fallthrough
+	case "OPEN":
+		fallthrough
+	case "TOGGLE":
+		fallthrough
+	case "PUSH":
+		return true, nil
+
+	case "OFF":
+		fallthrough
+	case "DOWN":
+		fallthrough
+	case "UNLOCK":
+		fallthrough
+	case "CLOSE":
+		return false, nil
+	}
+
+	// Command didn't match any of the above, get out of here
+	return false, fmt.Errorf("Error: %s in an invalid command", request)
 }
