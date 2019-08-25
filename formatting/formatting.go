4053197422433c1c39558039ef26d351b4b365e8
Quinn Casey <qcasey1@gmail.com>
2019-08-25 15:32:58 -0700

    adding time formats

--- formatting/formatting.go
+++ formatting/formatting.go
@@ -4,6 +4,7 @@ package formatting
 import (
 	"fmt"
 	"strings"
+	"time"
 	"unicode"
 )
 
@@ -50,4 +51,29 @@ func IsPositiveRequest(request string) (bool, error) {
 
 	// Command didn't match any of the above, get out of here
 	return false, fmt.Errorf("Error: %s in an invalid command", request)
+}
+
+// CompareTimestamps assuming both timezones are the same
+func CompareTimestamps(time1 string, time2 string) (time.Duration, error) {
+	t, err := time.Parse("2006-01-02 15:04:05.999", time2)
+	t2, err2 := time.Parse("2006-01-02 15:04:05.999", time2)
+
+	if err != nil {
+		return 0, err
+	}
+	if err2 != nil {
+		return 0, err2
+	}
+
+	return t.Sub(t2), nil
+}
+
+// CompareTimeToNow given a properly formatted time and timezone
+func CompareTimeToNow(time1 string, timezone *time.Location) (time.Duration, error) {
+	t, err := time.Parse("2006-01-02 15:04:05.999", time1)
+
+	if err != nil {
+		return 0, err
+	}
+	return time.Now().In(timezone).Sub(t), nil
 }
