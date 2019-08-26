e9f5409e0bd39ef9943c66dc6a08de350e656565
Quinn Casey <qcasey1@gmail.com>
2019-08-25 21:02:45 -0700

    Refactoring config

--- config.go
+++ config.go
@@ -20,43 +20,37 @@ import (
 // ConfigValues controls program settings and general persistent settings
 type ConfigValues struct {
 	AuthToken             string
+	BluetoothAddress      string
+	BluetoothEnabled      bool
+	DB                    *influx.Influx
 	DatabaseEnabled       bool
 	DatabaseHost          string
 	DatabaseName          string
-	BluetoothAddress      string
-	BluetoothEnabled      bool
-	PingHost              string
 	DebugSessionFile      string
-	SlackURL              string
 	HardwareSerialEnabled bool
 	HardwareSerialPort    string
 	HardwareSerialBaud    string
+	PingHost              string
 	SerialControlDevice   *serial.Port
+	SettingsFile          string
+	SessionFile           string
+	SlackURL              string
 	VerboseOutput         bool
 }
 
 // Config defined here, to be saved to below
 var Config ConfigValues
 
-// DB for influx, that may or may not be used globally
-var DB influx.Influx
-
 // Main config parsing
 func parseConfig() {
 
-	// Start with program arguments
-	var (
-		settingsFile string
-		sessionFile  string // This is for debugging ONLY
-	)
-	flag.StringVar(&settingsFile, "settings-file", "", "File to recover the persistent settings.")
-	flag.StringVar(&sessionFile, "session-file", "", "[DEBUG ONLY] File to save and recover the last-known session.")
+	flag.StringVar(&Config.SettingsFile, "settings-file", "", "File to recover the persistent settings.")
+	flag.StringVar(&Config.SessionFile, "session-file", "", "[DEBUG ONLY] File to save and recover the last-known session.")
 	flag.Parse()
 
 	// Parse settings file
-	settingsData, VerboseOutput := settings.SetupSettings(settingsFile)
+	settingsData, VerboseOutput := settings.SetupSettings(Config.SettingsFile)
 	Config.VerboseOutput = VerboseOutput
-	SetupSessions(sessionFile)
 
 	// Log settings
 	out, err := json.Marshal(settingsData)
@@ -67,53 +61,15 @@ func parseConfig() {
 
 	// Init session tracking (with or without Influx)
 	// Fetch and append old session from disk if allowed
+	SetupSessions(Config.SettingsFile)
 
 	// Parse through config if found in settings file
 	configMap, ok := settingsData["MDROID"]
 	if ok {
 
-		// Set up timezone
-		timezoneLocation, usingTimezone := configMap["Timezone"]
-		if usingTimezone {
-			loc, err := time.LoadLocation(timezoneLocation)
-			if err == nil {
-				Timezone = loc
-			} else {
-				// If timezone has errored
-				Timezone, _ = time.LoadLocation("UTC")
-			}
-		} else {
-			// If timezone is not set in config
-			Timezone, _ = time.LoadLocation("UTC")
-		}
-
-		// Set up InfluxDB time series logging
-		databaseHost, usingDatabase := configMap["DATABASE_HOST"]
-		if usingDatabase {
-			DB = influx.Influx{Host: databaseHost, Database: configMap["DATABASE_NAME"]}
-			Config.DatabaseEnabled = true
-
-			// Set up ping functionality
-			// Proprietary pinging for component tracking
-			if configMap["PING_HOST"] != "" {
-				logging.RemotePingAddress = configMap["PING_HOST"]
-			} else {
-				MainStatus.Log(logging.OK(), "[DISABLED] Not forwarding pings to host")
-			}
-
-		} else {
-			Config.DatabaseEnabled = false
-			MainStatus.Log(logging.OK(), "[DISABLED] Not logging to influx db")
-		}
-
-		// Set up bluetooth
-		bluetoothAddress, usingBluetooth := configMap["BLUETOOTH_ADDRESS"]
-		if usingBluetooth {
-			bluetooth.EnableAutoRefresh()
-			bluetooth.SetAddress(bluetoothAddress)
-			Config.BluetoothAddress = bluetoothAddress
-		}
-		Config.BluetoothEnabled = usingBluetooth
+		setupTimezone(&configMap)
+		setupDatabase(&configMap)
+		setupBluetooth(&configMap)
 
 		// Set up pybus repeat commands
 		_, usingPybus := configMap["PYBUS_DEVICE"]
@@ -138,35 +94,93 @@ func parseConfig() {
 			Config.AuthToken = authToken
 		}
 
-		//
-		// PROPRIETARY
-		// Configure hardware serials, should not be used outside my own config
-		//
-		HardwareSerialPort, usingHardwareSerial := configMap["HARDWARE_SERIAL_PORT"]
-		hardwareSerialBaud, usingHardwareBaud := configMap["HARDWARE_SERIAL_BAUD"]
-		Config.HardwareSerialEnabled = usingHardwareSerial
-		if Config.HardwareSerialEnabled {
-			// Configure default baudrate
-			HardwareSerialBaud := 9600
-			if usingHardwareBaud {
-				baudrateString, err := strconv.Atoi(hardwareSerialBaud)
-				if err != nil {
-					MainStatus.Log(logging.Error(), "Failed to convert HardwareSerialBaud to int. Found value: "+hardwareSerialBaud)
-					MainStatus.Log(logging.Warning(), "Disabling hardware serial functionality")
-					Config.HardwareSerialEnabled = false
-					return
-				}
+		setupSerial(&settingsData)
 
-				HardwareSerialBaud = baudrateString
-			}
-			// Start initial reader / writer
-			StartSerialComms(HardwareSerialPort, HardwareSerialBaud)
-
-			// Setup other devices
-			parseSerialDevices(settingsData)
-		}
 	} else {
 		MainStatus.Log(logging.Warning(), "No config found in settings file, not parsing through config")
+	}
+}
+
+func setupTimezone(configAddr *map[string]string) {
+	configMap := *configAddr
+	timezoneLocation, usingTimezone := configMap["Timezone"]
+	if usingTimezone {
+		loc, err := time.LoadLocation(timezoneLocation)
+		if err == nil {
+			Timezone = loc
+		} else {
+			// If timezone has errored
+			Timezone, _ = time.LoadLocation("UTC")
+		}
+	} else {
+		// If timezone is not set in config
+		Timezone, _ = time.LoadLocation("UTC")
+	}
+}
+
+// Set up InfluxDB time series logging
+func setupDatabase(configAddr *map[string]string) {
+	configMap := *configAddr
+	databaseHost, usingDatabase := configMap["DATABASE_HOST"]
+	if usingDatabase {
+		Config.DB = &influx.Influx{Host: databaseHost, Database: configMap["DATABASE_NAME"]}
+		Config.DatabaseEnabled = true
+
+		// Set up ping functionality
+		// Proprietary pinging for component tracking
+		if configMap["PING_HOST"] != "" {
+			logging.RemotePingAddress = configMap["PING_HOST"]
+		} else {
+			MainStatus.Log(logging.OK(), "[DISABLED] Not forwarding pings to host")
+		}
+
+	} else {
+		Config.DatabaseEnabled = false
+		MainStatus.Log(logging.OK(), "[DISABLED] Not logging to influx db")
+	}
+}
+
+func setupBluetooth(configAddr *map[string]string) {
+	configMap := *configAddr
+	bluetoothAddress, usingBluetooth := configMap["BLUETOOTH_ADDRESS"]
+	if usingBluetooth {
+		bluetooth.EnableAutoRefresh()
+		bluetooth.SetAddress(bluetoothAddress)
+		Config.BluetoothAddress = bluetoothAddress
+	}
+	Config.BluetoothEnabled = usingBluetooth
+}
+
+//
+// PROPRIETARY
+// Configure hardware serials, should not be used outside my own config
+//
+func setupSerial(configAddr *map[string]map[string]string) {
+	settingsData := *configAddr
+	configMap := settingsData["MDROID"]
+	HardwareSerialPort, usingHardwareSerial := configMap["HARDWARE_SERIAL_PORT"]
+	hardwareSerialBaud, usingHardwareBaud := configMap["HARDWARE_SERIAL_BAUD"]
+	Config.HardwareSerialEnabled = usingHardwareSerial
+
+	if Config.HardwareSerialEnabled {
+		// Configure default baudrate
+		HardwareSerialBaud := 9600
+		if usingHardwareBaud {
+			baudrateString, err := strconv.Atoi(hardwareSerialBaud)
+			if err != nil {
+				MainStatus.Log(logging.Error(), "Failed to convert HardwareSerialBaud to int. Found value: "+hardwareSerialBaud)
+				MainStatus.Log(logging.Warning(), "Disabling hardware serial functionality")
+				Config.HardwareSerialEnabled = false
+				return
+			}
+
+			HardwareSerialBaud = baudrateString
+		}
+		// Start initial reader / writer
+		StartSerialComms(HardwareSerialPort, HardwareSerialBaud)
+
+		// Setup other devices
+		parseSerialDevices(settingsData)
 	}
 }
 
