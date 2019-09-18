89f2662860157c01d0c85d623bbb495cfb520171
Quinn Casey <qcasey1@gmail.com>
2019-09-17 20:39:20 -0700

    converting to floats

--- gps.go
+++ gps.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 	"strings"
 	"time"
 
@@ -12,15 +13,15 @@ import (
 
 // GPSData holds various data points we expect to receive
 type GPSData struct {
-	Latitude  string   `json:"latitude,omitempty"`
-	Longitude string   `json:"longitude,omitempty"`
-	Time      string   `json:"time,omitempty"` // This will help measure latency :)
-	Altitude  *float32 `json:"altitude,omitempty"`
-	EPV       *float32 `json:"epv,omitempty"`
-	EPT       *float32 `json:"ept,omitempty"`
-	Speed     *float32 `json:"speed,omitempty"`
-	Climb     *float32 `json:"climb,omitempty"`
-	Course    *float32 `json:"course,omitempty"`
+	Latitude  string `json:"latitude,omitempty"`
+	Longitude string `json:"longitude,omitempty"`
+	Time      string `json:"time,omitempty"` // This will help measure latency :)
+	Altitude  string `json:"altitude,omitempty"`
+	EPV       string `json:"epv,omitempty"`
+	EPT       string `json:"ept,omitempty"`
+	Speed     string `json:"speed,omitempty"`
+	Climb     string `json:"climb,omitempty"`
+	Course    string `json:"course,omitempty"`
 }
 
 // GPS is the last posted GPS fix
@@ -62,33 +63,37 @@ func SetGPSValue(w http.ResponseWriter, r *http.Request) {
 		GPS.Longitude = newdata.Longitude
 		postingString.WriteString(fmt.Sprintf("longitude=\"%s\",", newdata.Longitude))
 	}
-	if newdata.Altitude != nil {
+	if newdata.Altitude != "" {
 		GPS.Altitude = newdata.Altitude
-		postingString.WriteString(fmt.Sprintf("altitude=%f,", *newdata.Altitude))
+		convFloat, _ := strconv.ParseFloat(newdata.Altitude, 32)
+		postingString.WriteString(fmt.Sprintf("altitude=%f,", convFloat))
 	}
-	if newdata.Speed != nil {
+	if newdata.Speed != "" {
 		GPS.Speed = newdata.Speed
-		postingString.WriteString(fmt.Sprintf("speed=%f,", *newdata.Speed))
+		convFloat, _ := strconv.ParseFloat(newdata.Speed, 32)
+		postingString.WriteString(fmt.Sprintf("speed=%f,", convFloat))
 	}
-	if newdata.Climb != nil {
+	if newdata.Climb != "" {
 		GPS.Climb = newdata.Climb
-		postingString.WriteString(fmt.Sprintf("climb=%f,", *newdata.Climb))
+		convFloat, _ := strconv.ParseFloat(newdata.Climb, 32)
+		postingString.WriteString(fmt.Sprintf("climb=%f,", convFloat))
 	}
 	if newdata.Time == "" {
 		newdata.Time = time.Now().In(Timezone).Format("2006-01-02 15:04:05.999")
 	}
 	GPS.Time = newdata.Time
-	if newdata.EPV != nil {
+	if newdata.EPV != "" {
 		GPS.EPV = newdata.EPV
-		postingString.WriteString(fmt.Sprintf("EPV=%f,", *newdata.EPV))
+		postingString.WriteString(fmt.Sprintf("EPV=%s,", newdata.EPV))
 	}
-	if newdata.EPT != nil {
+	if newdata.EPT != "" {
 		GPS.EPT = newdata.EPT
-		postingString.WriteString(fmt.Sprintf("EPT=%f,", *newdata.EPT))
+		postingString.WriteString(fmt.Sprintf("EPT=%s,", newdata.EPT))
 	}
-	if newdata.Course != nil {
+	if newdata.Course != "" {
 		GPS.Course = newdata.Course
-		postingString.WriteString(fmt.Sprintf("Course=%f,", *newdata.Course))
+		convFloat, _ := strconv.ParseFloat(newdata.Course, 32)
+		postingString.WriteString(fmt.Sprintf("Course=%f,", convFloat))
 	}
 
 	// Insert into database
