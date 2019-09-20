462fddf1943d6eee3a8d75d3aa3726b71dd06c32
Quinn Casey <qcasey1@gmail.com>
2019-09-19 21:41:37 -0700

    move some service routes into main

--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"os/exec"
 	"time"
 
+	"github.com/MrDoctorKovacic/MDroid-Core/formatting"
 	"github.com/MrDoctorKovacic/MDroid-Core/logging"
+	"github.com/MrDoctorKovacic/MDroid-Core/settings"
 )
 
 // MainStatus will control logging and reporting of status / warnings / errors
@@ -20,4 +27,44 @@ func main() {
 
 	// Define routes and begin routing
 	startRouter()
+}
+
+// **
+// Main service routes
+// **
+
+// Stop MDroid-Core service
+func stopMDroid(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode("OK")
+	MainStatus.Log(logging.OK(), "Stopping MDroid Service")
+	os.Exit(0)
+}
+
+// Reboot the machine
+func rebootMDroid(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode("OK")
+	exec.Command("reboot", "now")
+}
+
+// Shutdown the current machine
+func shutdownMDroid(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode("OK")
+	exec.Command("poweroff", "now")
+}
+
+// Send a command to a network machine, using a simple python server to recieve
+func commandNetworkMachine(name string, command string) {
+	machineServiceAddress, err := settings.GetSettingByName(formatting.FormatName(name), "ADDRESS")
+
+	resp, err := http.Get(fmt.Sprintf("http://%s:5350/%s", machineServiceAddress, command))
+	if err != nil {
+		MainStatus.Log(logging.Error(), fmt.Sprintf("Failed to command machine %s (at %s) to %s: \n%s", name, machineServiceAddress, command, err.Error()))
+		return
+	}
+	defer resp.Body.Close()
+}
+
+// welcomeRoute intros MDroid-Core, proving port and service works
+func welcomeRoute(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode("Welcome to MDroid! This port is fully operational, see the docs for applicable routes.")
 }
