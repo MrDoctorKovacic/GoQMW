5546506725c5f3d7d625a295704dff34f7afb7b1
Quinn Casey <qcasey1@gmail.com>
2019-09-25 16:27:53 -0700

    changing ping address

--- sessions/socket_external.go
+++ sessions/socket_external.go
@@ -3,13 +3,46 @@ package sessions
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"net/url"
 	"time"
 
+	"github.com/MrDoctorKovacic/MDroid-Core/logging"
 	"github.com/gorilla/websocket"
 )
 
 var clientConnected bool
+
+// CheckServer will continiously ping a central server for waiting packets,
+// and will open a websocket as a client if so
+func (session *Session) CheckServer(host string, token string) {
+	var timeToWait time.Duration
+	for {
+		lteEnabled, err := session.GetSessionValue("LTE_ON")
+		if err != nil {
+			SessionStatus.Log(logging.Error(), "Error getting LTE status.")
+			timeToWait = time.Second * 5
+		} else if lteEnabled.Value == "TRUE" {
+			// Slow frequency of pings while on LTE
+			timeToWait = time.Second * 5
+		} else {
+			// We can assume we're not on LTE, lower the wait time
+			timeToWait = time.Second * 1
+		}
+
+		resp, err := http.Get(fmt.Sprintf("http://%s/ping", host))
+		if err != nil {
+			// handle error
+			SessionStatus.Log(logging.Error(), fmt.Sprintf("Error when pinging the central server.\n%s", err.Error()))
+		}
+		resp.Body.Close()
+		if resp.StatusCode == 200 {
+			requestServerSocket(host, token)
+		}
+
+		time.Sleep(timeToWait)
+	}
+}
 
 func requestServerSocket(host string, token string) {
 	// Copyright 2015 The Gorilla WebSocket Authors. All rights reserved.
