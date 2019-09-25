b7c34ffebee3b5ec83f088fcd213ab3d3bf23c7c
Quinn Casey <qcasey1@gmail.com>
2019-09-25 16:07:04 -0700

    updating websocket loop

--- sessions/websockets.go
+++ sessions/websockets.go
@@ -52,30 +52,31 @@ func (session *Session) GetSessionSocket(w http.ResponseWriter, r *http.Request)
 // CheckServer will continiously ping a central server for waiting packets,
 // and will open a websocket as a client if so
 func (session *Session) CheckServer(host string, token string) {
-	lteEnabled, err := session.GetSessionValue("LTE_ON")
-	if err != nil {
-		SessionStatus.Log(logging.Error(), "Error getting LTE status.")
-		return
-	}
-	for lteEnabled.Value == "TRUE" {
-		lteEnabled, err = session.GetSessionValue("LTE_ON")
+	var timeToWait time.Duration
+	for {
+		lteEnabled, err := session.GetSessionValue("LTE_ON")
 		if err != nil {
 			SessionStatus.Log(logging.Error(), "Error getting LTE status.")
-			return
+			timeToWait = time.Second * 5
+		} else if lteEnabled.Value == "TRUE" {
+			// Slow frequency of pings while on LTE
+			timeToWait = time.Second * 5
+		} else {
+			// We can assume we're not on LTE, lower the wait time
+			timeToWait = time.Second * 1
 		}
 
 		resp, err := http.Get(fmt.Sprintf("%s/ping", host))
 		if err != nil {
 			// handle error
 			SessionStatus.Log(logging.Error(), fmt.Sprintf("Error when pinging the central server.\n%s", err.Error()))
-			return
 		}
 		resp.Body.Close()
 		if resp.StatusCode == 200 {
 			requestServerSocket(fmt.Sprintf("%s/%s", host, token))
 		}
 
-		time.Sleep(time.Second * 10)
+		time.Sleep(timeToWait)
 	}
 }
 
