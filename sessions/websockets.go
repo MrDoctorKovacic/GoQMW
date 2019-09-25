baa43a0a39a2451c197081f7f2f470b9affe7e3c
Quinn Casey <qcasey1@gmail.com>
2019-09-25 13:44:21 -0700

    initial support for central sockets

--- sessions/websockets.go
+++ sessions/websockets.go
@@ -1,10 +1,16 @@
 package sessions
 
 import (
+	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/MrDoctorKovacic/MDroid-Core/logging"
+	"github.com/gorilla/websocket"
 )
+
+// Session WebSocket upgrader
+var upgrader = websocket.Upgrader{} // use default options
 
 // GetSessionSocket returns the entire current session as a webstream
 func (session *Session) GetSessionSocket(w http.ResponseWriter, r *http.Request) {
@@ -41,4 +47,38 @@ func (session *Session) GetSessionSocket(w http.ResponseWriter, r *http.Request)
 			break
 		}
 	}
+}
+
+// CheckServer will continiously ping a central server for waiting packets,
+// and will open a websocket as a client if so
+func (session *Session) CheckServer(host string, token string) {
+	lteEnabled, err := session.GetSessionValue("LTE_ON")
+	if err != nil {
+		SessionStatus.Log(logging.Error(), "Error getting LTE status.")
+		return
+	}
+	for lteEnabled.Value == "TRUE" {
+		lteEnabled, err = session.GetSessionValue("LTE_ON")
+		if err != nil {
+			SessionStatus.Log(logging.Error(), "Error getting LTE status.")
+			return
+		}
+
+		resp, err := http.Get(fmt.Sprintf("%s/ping", host))
+		if err != nil {
+			// handle error
+			SessionStatus.Log(logging.Error(), fmt.Sprintf("Error when pinging the central server.\n%s", err.Error()))
+			return
+		}
+		resp.Body.Close()
+		if resp.StatusCode == 200 {
+			requestServerSocket(fmt.Sprintf("%s/%s", host, token))
+		}
+
+		time.Sleep(time.Second * 10)
+	}
+}
+
+func requestServerSocket(host string) {
+
 }
