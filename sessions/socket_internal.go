514907e75b3807a2b5fa21bd0105ae240c068a62
Quinn Casey <qcasey1@gmail.com>
2019-09-25 16:27:53 -0700

    changing ping address

--- sessions/socket_internal.go
+++ sessions/socket_internal.go
@@ -1,9 +1,7 @@
 package sessions
 
 import (
-	"fmt"
 	"net/http"
-	"time"
 
 	"github.com/MrDoctorKovacic/MDroid-Core/logging"
 	"github.com/gorilla/websocket"
@@ -46,36 +44,5 @@ func (session *Session) GetSessionSocket(w http.ResponseWriter, r *http.Request)
 			SessionStatus.Log(logging.Error(), "Error writing to webstream: "+err.Error())
 			break
 		}
-	}
-}
-
-// CheckServer will continiously ping a central server for waiting packets,
-// and will open a websocket as a client if so
-func (session *Session) CheckServer(host string, token string) {
-	var timeToWait time.Duration
-	for {
-		lteEnabled, err := session.GetSessionValue("LTE_ON")
-		if err != nil {
-			SessionStatus.Log(logging.Error(), "Error getting LTE status.")
-			timeToWait = time.Second * 5
-		} else if lteEnabled.Value == "TRUE" {
-			// Slow frequency of pings while on LTE
-			timeToWait = time.Second * 5
-		} else {
-			// We can assume we're not on LTE, lower the wait time
-			timeToWait = time.Second * 1
-		}
-
-		resp, err := http.Get(fmt.Sprintf("ws://%s/ping", host))
-		if err != nil {
-			// handle error
-			SessionStatus.Log(logging.Error(), fmt.Sprintf("Error when pinging the central server.\n%s", err.Error()))
-		}
-		resp.Body.Close()
-		if resp.StatusCode == 200 {
-			requestServerSocket(host, token)
-		}
-
-		time.Sleep(timeToWait)
 	}
 }
