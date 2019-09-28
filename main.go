d83c94d353e32cac418ed7703ff2d71b4301e36e
Quinn Casey <qcasey1@gmail.com>
2019-09-28 10:53:23 -0700

    shuffling routes

--- main.go
+++ main.go
@@ -1,18 +1,15 @@
 package main
 
 import (
-	"encoding/json"
-	"net/http"
-	"os"
-
-	"github.com/MrDoctorKovacic/MDroid-Core/formatting"
 	"github.com/MrDoctorKovacic/MDroid-Core/logging"
-	"github.com/MrDoctorKovacic/MDroid-Core/mserial"
-	"github.com/gorilla/mux"
 )
 
 // mainStatus will control logging and reporting of status / warnings / errors
-var mainStatus = logging.NewStatus("Main")
+var mainStatus logging.ProgramStatus
+
+func init() {
+	mainStatus = logging.NewStatus("Main")
+}
 
 // define our router and subsequent routes here
 func main() {
@@ -22,43 +19,4 @@ func main() {
 
 	// Define routes and begin routing
 	startRouter()
-}
-
-// **
-// Main service routes
-// **
-
-// Stop MDroid-Core service
-func stopMDroid(w http.ResponseWriter, r *http.Request) {
-	mainStatus.Log(logging.OK(), "Stopping MDroid Service as per request")
-	json.NewEncoder(w).Encode(formatting.JSONResponse{Output: "OK", Status: "success", OK: true})
-	os.Exit(0)
-}
-
-// Reboot the machine
-func handleReboot(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	machine, ok := params["machine"]
-
-	if !ok {
-		json.NewEncoder(w).Encode(formatting.JSONResponse{Output: "Machine name required", Status: "fail", OK: false})
-		return
-	}
-
-	json.NewEncoder(w).Encode(formatting.JSONResponse{Output: "OK", Status: "success", OK: true})
-	mserial.CommandNetworkMachine(machine, "reboot")
-}
-
-// Shutdown the current machine
-func handleShutdown(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	machine, ok := params["machine"]
-
-	if !ok {
-		json.NewEncoder(w).Encode(formatting.JSONResponse{Output: "Machine name required", Status: "fail", OK: false})
-		return
-	}
-
-	json.NewEncoder(w).Encode(formatting.JSONResponse{Output: "OK", Status: "success", OK: true})
-	mserial.CommandNetworkMachine(machine, "shutdown")
 }
