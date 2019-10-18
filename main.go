58180f8a1904b6f79e9272e831c28110a42b2c82
Quinn Casey <qcasey1@gmail.com>
2019-10-18 11:40:40 -0700

    update name

--- main.go
+++ main.go
@@ -41,7 +41,7 @@ func gracefulShutdown(name string) {
 
 // sendServiceCommand sends a command to a network machine, using a simple python server to recieve
 func sendServiceCommand(name string, command string) {
-	machineServiceAddress, err := settings.Get(formatting.FormatName(name), "ADDRESS")
+	machineServiceAddress, err := settings.Get(formatting.Name(name), "ADDRESS")
 	if machineServiceAddress == "" {
 		return
 	}
