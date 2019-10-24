da40ba81741dd0d8f2bf676d233ed845711dc018
Quinn <qcasey1@gmail.com>
2019-10-23 17:51:27 -0700

    move function calls

--- bluetooth/bluetooth.go
+++ bluetooth/bluetooth.go
@@ -3,7 +3,6 @@ package bluetooth
 
 import (
 	"bytes"
-	"encoding/json"
 	"fmt"
 	"net/http"
 	"os/exec"
@@ -168,7 +167,7 @@ func Connect(w http.ResponseWriter, r *http.Request) {
 
 	var stderr bytes.Buffer
 	var out bytes.Buffer
-	cmd := exec.Command("/bin/sh", "/home/pi/bluetooth/connect.sh")
+	cmd := exec.Command("/bin/sh", "/home/casey/MDroid/bluetooth/connect.sh")
 	cmd.Stdout = &out
 	cmd.Stderr = &stderr
 
@@ -186,7 +185,7 @@ func Disconnect(w http.ResponseWriter, r *http.Request) {
 
 	var stderr bytes.Buffer
 	var out bytes.Buffer
-	cmd := exec.Command("/bin/sh", "/home/pi/bluetooth/disconnect.sh")
+	cmd := exec.Command("/bin/sh", "/home/casey/MDroid/bluetooth/disconnect.sh")
 	cmd.Stdout = &out
 	cmd.Stderr = &stderr
 
@@ -198,60 +197,73 @@ func Disconnect(w http.ResponseWriter, r *http.Request) {
 	format.WriteResponse(&w, r, format.JSONResponse{Output: "OK", OK: true})
 }
 
-// GetDeviceInfo attempts to get metadata about connected device
-func GetDeviceInfo(w http.ResponseWriter, r *http.Request) {
-	log.Info().Msg("Getting device info...")
-	result, ok := SendDBusCommand([]string{"/org/bluez/hci0/dev_" + BluetoothAddress + "/player0", "org.freedesktop.DBus.Properties.Get", "string:org.bluez.MediaPlayer1", "string:Status"}, true)
-	if !ok {
-		json.NewEncoder(w).Encode(format.JSONResponse{Output: "Error getting device info", Status: "fail", OK: false})
-		return
-	}
-	json.NewEncoder(w).Encode(format.JSONResponse{Output: cleanDBusOutput(result), Status: "success", OK: true})
-}
-
-// GetMediaInfo attempts to get metadata about current track
-func GetMediaInfo(w http.ResponseWriter, r *http.Request) {
+func askDeviceInfo() map[string]string {
 	log.Info().Msg("Getting device info...")
 
 	deviceMessage := []string{"/org/bluez/hci0/dev_" + BluetoothAddress + "/player0", "org.freedesktop.DBus.Properties.Get", "string:org.bluez.MediaPlayer1", "string:Status"}
 	result, ok := SendDBusCommand(deviceMessage, true)
 	if !ok {
-		json.NewEncoder(w).Encode(format.JSONResponse{Output: "Error getting media info", Status: "fail", OK: false})
-		return
+		return nil
 	}
 	if result == "" {
 		// empty response
 		log.Warn().Msg(fmt.Sprintf("Empty dbus response when querying device, not attempting to clean. We asked: \n%s", strings.Join(deviceMessage, " ")))
-		return
+		return nil
 	}
-	deviceStatus := cleanDBusOutput(result)
+	return cleanDBusOutput(result)
+}
 
+func askMediaInfo() map[string]string {
 	log.Info().Msg("Getting media info...")
 	mediaMessage := []string{"/org/bluez/hci0/dev_" + BluetoothAddress + "/player0", "org.freedesktop.DBus.Properties.Get", "string:org.bluez.MediaPlayer1", "string:Track"}
-	result, ok = SendDBusCommand(mediaMessage, true)
+	result, ok := SendDBusCommand(mediaMessage, true)
 	if !ok {
-		json.NewEncoder(w).Encode(format.JSONResponse{Output: "Error getting media info", Status: "fail", OK: false})
-		return
+		return nil
 	}
 	if result == "" {
 		// empty response
 		log.Warn().Msg(fmt.Sprintf("Empty dbus response when querying media, not attempting to clean. We asked: \n%s", strings.Join(mediaMessage, " ")))
+		return nil
+	}
+	return cleanDBusOutput(result)
+}
+
+// GetDeviceInfo attempts to get metadata about connected device
+func GetDeviceInfo(w http.ResponseWriter, r *http.Request) {
+	deviceStatus := askDeviceInfo()
+	if deviceStatus == nil {
+		format.WriteResponse(&w, r, format.JSONResponse{Output: "Error getting media info", Status: "fail", OK: false})
+		return
+	}
+	format.WriteResponse(&w, r, format.JSONResponse{Output: deviceStatus, Status: "success", OK: true})
+}
+
+// GetMediaInfo attempts to get metadata about current track
+func GetMediaInfo(w http.ResponseWriter, r *http.Request) {
+	deviceStatus := askDeviceInfo()
+	if deviceStatus == nil {
+		format.WriteResponse(&w, r, format.JSONResponse{Output: "Error getting media info", Status: "fail", OK: false})
+		return
+	}
+
+	response := askMediaInfo()
+	if response == nil {
+		format.WriteResponse(&w, r, format.JSONResponse{Output: "Error getting media info", Status: "fail", OK: false})
 		return
 	}
 
 	// Append device status to media info
-	cleanResult := cleanDBusOutput(result)
-	cleanResult["Status"] = deviceStatus["Meta"]
+	response["Status"] = deviceStatus["Meta"]
 
 	// Append Album / Artwork slug if both exist
-	album, albumOK := cleanResult["Album"]
-	artist, artistOK := cleanResult["Artist"]
+	album, albumOK := response["Album"]
+	artist, artistOK := response["Artist"]
 	if albumOK && artistOK {
-		cleanResult["Album_Artwork"] = slug.Make(artist) + "/" + slug.Make(album) + ".jpg"
+		response["Album_Artwork"] = slug.Make(artist) + "/" + slug.Make(album) + ".jpg"
 	}
 
 	// Echo back all info
-	json.NewEncoder(w).Encode(format.JSONResponse{Output: cleanResult, Status: "success", OK: true})
+	format.WriteResponse(&w, r, format.JSONResponse{Output: response, Status: "success", OK: true})
 }
 
 // Prev skips to previous track
