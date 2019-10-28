77e26855740d3970c8e42204ffda3fe0cf9a2b0f
MrDoctorKovacic <qcasey1@gmail.com>
2019-10-28 13:14:43 -0700

    move power function

--- main.go
+++ main.go
@@ -3,13 +3,10 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"time"
 
 	"github.com/MrDoctorKovacic/MDroid-Core/format"
-	"github.com/MrDoctorKovacic/MDroid-Core/mserial"
 	"github.com/MrDoctorKovacic/MDroid-Core/settings"
 	"github.com/rs/zerolog/log"
-	"github.com/tarm/serial"
 )
 
 // define our router and subsequent routes here
@@ -20,23 +17,6 @@ func main() {
 
 	// Define routes and begin routing
 	startRouter()
-}
-
-// Some shutdowns are more complicated than others, ensure we shut down safely
-func gracefulShutdown(name string) {
-	serialCommand := fmt.Sprintf("powerOff%s", name)
-
-	switch name {
-	case "Board":
-		sendServiceCommand("etc", "shutdown")
-		machineShutdown(mserial.Writer, "board", time.Second*10, serialCommand)
-	case "Wireless":
-		machineShutdown(mserial.Writer, "lte", time.Second*10, serialCommand)
-	case "Sound":
-		machineShutdown(mserial.Writer, "sound", time.Second*10, serialCommand)
-	default:
-		mserial.Push(mserial.Writer, serialCommand)
-	}
 }
 
 // sendServiceCommand sends a command to a network machine, using a simple python server to recieve
@@ -52,11 +32,4 @@ func sendServiceCommand(name string, command string) {
 		return
 	}
 	defer resp.Body.Close()
-}
-
-// machineShutdown shutdowns the named machine safely
-func machineShutdown(serialDevice *serial.Port, machine string, timeToSleep time.Duration, serialMessage string) {
-	sendServiceCommand(machine, "shutdown")
-	time.Sleep(timeToSleep)
-	mserial.Push(serialDevice, serialMessage)
 }
