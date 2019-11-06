5056d78b8f87964d1daabca1964842b175581bfe
Quinn <qcasey1@gmail.com>
2019-11-05 16:52:37 -0800

    use global defs in functions

--- power.go
+++ power.go
@@ -24,54 +24,50 @@ type power struct {
 
 // Read the target action based on current ACC Power value
 var (
-	_wirelessDef = power{settingComp: "WIRELESS", settingName: "POWER"}
-	_angelDef    = power{settingComp: "ANGEL_EYES", settingName: "POWER"}
-	_tabletDef   = power{settingComp: "TABLET", settingName: "POWER"}
-	_boardDef    = power{settingComp: "BOARD", settingName: "POWER"}
+	_wireless = power{settingComp: "WIRELESS", settingName: "POWER"}
+	_angel    = power{settingComp: "ANGEL_EYES", settingName: "POWER"}
+	_tablet   = power{settingComp: "TABLET", settingName: "POWER"}
+	_board    = power{settingComp: "BOARD", settingName: "POWER"}
 )
 
 // Evaluates if the angel eyes should be on, and then passes that struct along as generic power module
 func evalAngelEyesPower(keyIsIn string) {
-	angel := _angelDef
-	angel.on, angel.errOn = sessions.GetBool("ANGEL_EYES_POWER")
-	angel.powerTarget, angel.errTarget = settings.Get(angel.settingComp, angel.settingName)
+	_angel.on, _angel.errOn = sessions.GetBool("ANGEL_EYES_POWER")
+	_angel.powerTarget, _angel.errTarget = settings.Get(_angel.settingComp, _angel.settingName)
 	lightSensor := sessions.GetBoolDefault("LIGHT_SENSOR_ON", false)
 
 	shouldTrigger := !lightSensor && keyIsIn != "FALSE"
 
 	// Pass angel module to generic power trigger
-	genericPowerTrigger(shouldTrigger, "Angel", angel)
+	genericPowerTrigger(shouldTrigger, "Angel", &_angel)
 }
 
 // Evaluates if the video boards should be on, and then passes that struct along as generic power module
 func evalVideoPower(keyIsIn string, accOn bool, wifiOn bool) {
-	board := _boardDef
-	board.on, board.errOn = sessions.GetBool("BOARD_POWER")
-	board.powerTarget, board.errTarget = settings.Get(board.settingComp, board.settingName)
+	_board.on, _board.errOn = sessions.GetBool("BOARD_POWER")
+	_board.powerTarget, _board.errTarget = settings.Get(_board.settingComp, _board.settingName)
 
 	shouldTrigger := accOn && !wifiOn || wifiOn && keyIsIn != "FALSE"
 
 	// Pass angel module to generic power trigger
-	genericPowerTrigger(shouldTrigger, "Board", board)
+	genericPowerTrigger(shouldTrigger, "Board", &_board)
 }
 
 // Evaluates if the tablet should be on, and then passes that struct along as generic power module
 func evalTabletPower(keyIsIn string, accOn bool, wifiOn bool) {
-	tablet := _tabletDef
-	tablet.on, tablet.errOn = sessions.GetBool("TABLET_POWER")
-	tablet.powerTarget, tablet.errTarget = settings.Get(tablet.settingComp, tablet.settingName)
+	_tablet.on, _tablet.errOn = sessions.GetBool("TABLET_POWER")
+	_tablet.powerTarget, _tablet.errTarget = settings.Get(_tablet.settingComp, _tablet.settingName)
 
 	shouldTrigger := accOn && !wifiOn || wifiOn && keyIsIn != "FALSE"
 
 	// Pass angel module to generic power trigger
-	genericPowerTrigger(shouldTrigger, "Tablet", tablet)
+	genericPowerTrigger(shouldTrigger, "Tablet", &_tablet)
 }
 
 // Evaluates if the wireless boards should be on, and then passes that struct along as generic power module
 func evalWirelessPower(keyIsIn string, accOn bool, wifiOn bool) {
-	wireless := _wirelessDef
-	wireless.on, wireless.errOn = sessions.GetBool("WIRELESS_POWER")
-	wireless.powerTarget, wireless.errTarget = settings.Get(wireless.settingComp, wireless.settingName)
+	_wireless.on, _wireless.errOn = sessions.GetBool("WIRELESS_POWER")
+	_wireless.powerTarget, _wireless.errTarget = settings.Get(_wireless.settingComp, _wireless.settingName)
 
 	// Wireless is most likely supposed to be on, only one case where it should not be
 	shouldTrigger := true
@@ -80,11 +76,11 @@ func evalWirelessPower(keyIsIn string, accOn bool, wifiOn bool) {
 	}
 
 	// Pass wireless module to generic power trigger
-	genericPowerTrigger(shouldTrigger, "Wireless", wireless)
+	genericPowerTrigger(shouldTrigger, "Wireless", &_wireless)
 }
 
 // Error check against module's status fetches, then check if we're powering on or off
-func genericPowerTrigger(shouldBeOn bool, name string, module power) {
+func genericPowerTrigger(shouldBeOn bool, name string, module *power) {
 	// Handle error in fetches
 	if module.errTarget != nil {
 		log.Error().Msg(fmt.Sprintf("Setting Error: %s", module.errTarget.Error()))
