1246b97879e96b658b64c3aea93dcbc36c6ab640
Quinn <qcasey1@gmail.com>
2019-11-27 12:12:51 -0800

    use key detected for repeat commands

--- pybus/pybus.go
+++ pybus/pybus.go
@@ -71,7 +71,7 @@ func repeatCommand(command string, sleepSeconds int) {
 	log.Info().Msgf("Running Pybus command %s every %d seconds", command, sleepSeconds)
 	for {
 		// Only push repeated pybus commands when powered, otherwise the car won't sleep
-		if sessions.GetBoolDefault("ACC_POWER", false) {
+		if sessions.GetBoolDefault("KEY_DETECTED", false) {
 			PushQueue(command)
 		}
 		time.Sleep(time.Duration(sleepSeconds) * time.Second)
