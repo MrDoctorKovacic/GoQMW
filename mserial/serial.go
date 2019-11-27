cd94c20bb61378e07852af106e60d21978397225
Quinn <qcasey1@gmail.com>
2019-11-27 10:04:57 -0800

    remove check for existing command

--- mserial/serial.go
+++ mserial/serial.go
@@ -76,9 +76,7 @@ func Push(device *serial.Port, msg string) {
 		writeQueue[device] = []string{}
 	}
 
-	if !format.StringInSlice(msg, writeQueue[device]) {
-		writeQueue[device] = append(writeQueue[device], msg)
-	}
+	writeQueue[device] = append(writeQueue[device], msg)
 }
 
 // Pop the last message off the queue and write it to the respective serial
