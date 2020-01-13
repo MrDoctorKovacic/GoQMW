23259e042cae74e8a108a2c0b34f0c5270747c18
Quinn <qcasey1@gmail.com>
2020-01-12 19:19:01 -0800

    put modules into main

--- main.go
+++ main.go
@@ -1,14 +1,35 @@
 package main
 
+import (
+	"github.com/MrDoctorKovacic/MDroid-Core/sessions/system"
+	"github.com/gorilla/mux"
+	bluetooth "github.com/qcasey/MDroid-Bluetooth"
+	"github.com/qcasey/MDroid-Core/db"
+	"github.com/qcasey/MDroid-Core/mserial"
+	"github.com/qcasey/MDroid-Core/pybus"
+	"github.com/qcasey/MDroid-Core/sessions/gps"
+)
+
 func main() {
-	Start()
-}
+	// Init router
+	router := mux.NewRouter()
 
-// Start is exported for later modular usage
-func Start() {
-	// Run through the config file and set up some global variables
-	router := parseConfig()
+	// Run through the config file and retrieve some settings
+	configMap := parseConfig()
 
-	// Define routes and begin routing
-	startRouter(router)
+	// Setup conventional modules
+	// TODO: More modular handling of modules
+	mserial.Mod.Setup(configMap)
+	mserial.Mod.SetRoutes(router)
+	bluetooth.Mod.Setup(configMap)
+	bluetooth.Mod.SetRoutes(router)
+	gps.Mod.Setup(configMap)
+	gps.Mod.SetRoutes(router)
+	pybus.Mod.Setup(configMap)
+	pybus.Mod.SetRoutes(router)
+	system.Mod.Setup(configMap)
+	system.Mod.SetRoutes(router)
+	db.Mod.Setup(configMap)
+
+	Start(router)
 }
