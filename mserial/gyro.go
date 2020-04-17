1896b8109671ea3bc57409feee1e45041044a6d3
Quinn <qcasey1@gmail.com>
2020-04-17 09:49:44 -0700

    add log

--- mserial/gyro.go
+++ mserial/gyro.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/qcasey/MDroid-Core/format/response"
 	"github.com/qcasey/MDroid-Core/mqtt"
+	"github.com/rs/zerolog/log"
 )
 
 // Measurement contains a simple X,Y,Z output from the IMU
@@ -25,6 +26,7 @@ var currentGyroReading gyros
 
 // addMeasurement to current readings
 func addMeasurement(name string, m Measurement) error {
+	log.Info().Msg(name)
 	switch name {
 	case "ACCELERATION":
 		currentGyroReading.Acceleration = m
